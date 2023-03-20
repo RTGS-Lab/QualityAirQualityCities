8e71eaa3379541b29d6c435a1fbdea35e3ff4cb1
RwHendrickson <91440903+RwHendrickson@users.noreply.github.com>
2023-03-20 17:20:31 -0500

    Changed initialize_db.sql

--- database/initialize_db.sql
+++ database/initialize_db.sql
@@ -4,35 +4,68 @@
 
 CREATE DATABASE "MplsCommunityAir"; -- Create the database
 
--- \c "MplsCommunityAir"; -- Connect to database, Not sure this works in SQL
+\c "MplsCommunityAir"; -- Connect to database
 
 CREATE SCHEMA "PurpleAir"; -- Create Schema
 
 CREATE EXTENSION postgis; -- Add spatial extensions
 CREATE EXTENSION postgis_topology;
 
-CREATE TABLE PurpleAir.MplsPurpleAirSensors -- Create table to store information on each sensor
+CREATE TABLE PurpleAir.MPCA_Facilities -- Create table to store information on each facility
 (
-    sensor_id serial, -- Unique identifier
-	custodian_type text, -- Volunteer, business, MPRB (Parks & Rec), etc.
-	hardware text, -- info on hardware & model
-	firmware text, -- info on firmware
-	date_created date, -- Date of creation
-	last_modified date, -- Last time maintained
-	last_seen timestamp [ 0 ], -- last data submission
-	active bool, -- 0 means no longer active, 1 means active
-	altitude double precision,
-	geom geometry
+    FACILITY_ID int, -- Unique identifier
+    FACILITY_NAME text,
+    PERMIT_TYPE text,
+    INDUSTRY_TYPE text, 
+    NAICS_CODE int, -- a code to classify industry
+    COUNTY text,
+    LAST_REPORT int, -- last year of submission
+    geometry geometry
 );
 
-CREATE TABLE PurpleAir.MplsPurpleAirHistoric -- Create table to store historic readings
+CREATE TABLE PurpleAir.MPCA_Permitted_Emissions -- Create table to store historic facility emissions
 (
-    sensor_id int, -- Unique identifier
-    timestamp timestamp [ 0 ], -- Timestamp
-    a_sensor_reading float, --
-    a_sensor_error_flag bool,
-    b_sensor_reading float,
-    b_sensor_error_flag bool,
-    temperature float,
-    relative_humidity float
+    FACILITY_ID int, -- Unique identifier
+    YEAR int, 
+    POLLUTANT text,
+    LBS_EMITTED float
 );
+
+CREATE TABLE PurpleAir.MNDOT_Current_AADT_Segments -- Create table to store information on Current AADT segments
+( 
+    SEQUENCE_NUMBER int, -- Unique identifier
+    ROUTE_LABEL text,
+    STREET_NAME text,
+    DAILY_FACTOR text,
+    SEASONAL_FACTOR text,
+    AXLE_FACTOR text,
+    CURRENT_YEAR int,
+    CURRENT_VOLUME int,
+    geometry geometry
+);
+
+-- CREATE TABLE PurpleAir.:MplsPurpleAirSensors -- Create table to store information on each sensor
+-- (
+--     sensor_id serial, -- Unique identifier
+-- 	custodian_type text, -- Volunteer, business, MPRB (Parks & Rec), etc.
+-- 	hardware text, -- info on hardware & model
+-- 	firmware text, -- info on firmware
+-- 	date_created date, -- Date of creation
+-- 	last_modified date, -- Last time maintained
+-- 	last_seen timestamp [ 0 ], -- last data submission
+-- 	active bool, -- 0 means no longer active, 1 means active
+-- 	altitude double precision,
+-- 	geom geometry
+-- );
+-- 
+-- CREATE TABLE PurpleAir.:MplsPurpleAirHistoric -- Create table to store historic readings
+-- (
+--     sensor_id int, -- Unique identifier
+--     timestamp timestamp [ 0 ], -- Timestamp
+--     a_sensor_reading float, --
+--     a_sensor_error_flag bool,
+--     b_sensor_reading float,
+--     b_sensor_error_flag bool,
+--     temperature float,
+--     relative_humidity float
+-- );
