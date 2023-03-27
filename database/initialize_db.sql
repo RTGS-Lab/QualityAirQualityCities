a2dbc0be0347367cdb21c7d1bde7da3a95003c77
dunkelmodus <97997470+ThisFord@users.noreply.github.com>
2023-03-27 15:41:59 -0500

    initalize bug fix

--- database/initialize_db.sql
+++ database/initialize_db.sql
@@ -11,13 +11,6 @@ CREATE SCHEMA "PurpleAir"; -- Create Schema
 CREATE EXTENSION postgis; -- Add spatial extensions
 CREATE EXTENSION postgis_topology;
 
-CREATE TABLE PurpleAir.Minneapolis_Boundary
-(
-    CTU_ID int
-    CTU_NAME text
-    CTU_CODE text
-    geometry geometry
-);
 
 CREATE TABLE PurpleAir.MPCA_Facilities -- Create table to store information on each facility
 (
