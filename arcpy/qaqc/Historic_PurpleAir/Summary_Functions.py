c236e7f5d4dcd43da985ed1d27da34a6c9d0d00d
RwHendrickson <91440903+RwHendrickson@users.noreply.github.com>
2023-04-19 18:42:19 -0500

    Changed organization slightly

--- arcpy/qaqc/Historic_PurpleAir/Summary_Functions.py
+++ arcpy/qaqc/Historic_PurpleAir/Summary_Functions.py
@@ -1,10 +1,24 @@
 ### This script will store all of our Daily Summary Functions
 
-### Names of stats
+### Storage
 
-summary_stats = ['n_observations']
+summary_stats = [] # Stat Names
 
-summary_stats_dtypes = [int]
+summary_stats_dtypes = [] # Stat Value Types
+
+summary_stats_functions = [] # The functions
 
 ### Functions
+# These should return the stats as a list (even if it is just one stat)
 
+def n_observations(df):
+    
+    return [len(df)]
+
+# Add to storage
+
+summary_stats += ['n_observations'] # Stat Names
+
+summary_stats_dtypes += [int] # Stat Value Types
+
+summary_stats_functions += [n_observations]
