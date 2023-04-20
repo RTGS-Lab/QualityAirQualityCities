2380ebb5dc08a0918df52493d8d11e9b77e0cb34
L-roach <roach258@umn.edu>
2023-04-19 21:17:28 -0500

    Update Summary_Functions.py

--- arcpy/qaqc/Historic_PurpleAir/Summary_Functions.py
+++ arcpy/qaqc/Historic_PurpleAir/Summary_Functions.py
@@ -50,26 +50,53 @@ summary_stats_functions += [getminmax]
 
 # ~~~~~
 
-# Stats for Morning rush hour
+# Morning rush hour statistics
 
 df['timestamp'] = pd.to_datetime(df.timestamp)
 
-def pm_25_MorningRushStats(df):
+def pm25_morningRush_stats(df):
     df_6_to_9 = df.loc[(df['timestamp'].dt.hour >= 6) & (df['timestamp'].dt.hour < 9)]
     
-    pm25_morningRushmin = df_6_to_9['pm25'].min()
-    pm25_morningRushmax = df_6_to_9['pm25'].max()
-    pm25_morningRushmean = df_6_to_9['pm25'].mean()
-    pm25_morningRushstd = df_6_to_9['pm25'].std()
+    pm25_morningRush_min = df_6_to_9['pm25'].min()
+    pm25_morningRush_max = df_6_to_9['pm25'].max()
+    pm25_morningRush_mean = df_6_to_9['pm25'].mean()
+    pm25_morningRush_std = df_6_to_9['pm25'].std()
     
-    return [pm25_morningRushmean, pm25_morningRushmin, pm25_morningRushmax, pm25_morningRushstd]
+    return [pm25_morningRush_min, pm25_morningRush_max, pm25_morningRush_mean, pm25_morningRush_std]
 
 # Add to storage
 
-summary_stats_names += ['pm25_morningRushmean', 'pm25_morningRushmin', 'pm25_morningRushmax','pm25_morningRushstd'] # Stat Names
+summary_stats_names += ['pm25_morningRush_min', 'pm25_morningRush_max', 'pm25_morningRush_mean', 'pm25_morningRush_std'] # Stat Names
 
 summary_stats_dtypes += [float, float, float, float] # Stat Value Types
 
-summary_stats_functions += [pm_25_MorningRushStats]
+summary_stats_functions += [pm25_morningRush_stats]
 
+# ~~~~~
+#Daytime ambient statistics
+
+def pm25_daytimeAmbient_stats(df):
+    df['timestamp'] = pd.to_datetime(df.timestamp)
+    
+    #set the time range for the ambient daytime
+    start_daytime_ambient = pd.Timestamp('12:00:00')
+    end_daytime_ambient = pd.Timestamp('14:59:59')
+
+    #filter the df by this daytime ambient time range
+    filtered_daytime_ambient = df[(df['timestamp'].dt.time >= start_daytime_ambient.time()) & (df['timestamp'].dt.time <= end_daytime_ambient.time())]
+
+    #get the min, max, std, and average
+    pm25_daytimeAmbient_min = filtered_daytime_ambient['pm25'].min()
+    pm25_daytimeAmbient_max = filtered_daytime_ambient['pm25'].max()
+    pm25_daytimeAmbient_mean = filtered_daytime_ambient['pm25'].mean()
+    pm25_daytimeAmbient_std = filtered_daytime_ambient['pm25'].std()
+    
+    return [pm25_daytimeAmbient_min, pm25_daytimeAmbient_max, mean_dytm_amb, std_dev_dytm_amb]
+# Add to storage
+
+summary_stats_names += ['pm25_daytimeAmbient_min', 'pm25_daytimeAmbient_max', 'pm25_daytimeAmbient_mean','pm25_daytimeAmbient_std'] # Stat Names
+
+summary_stats_dtypes += [float, float, float, float] # Stat Value Types
+
+summary_stats_functions += [pm25_daytimeAmbient_stats]
 # ~~~~~
