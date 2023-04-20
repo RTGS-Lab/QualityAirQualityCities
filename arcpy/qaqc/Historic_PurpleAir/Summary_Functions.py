ddd28d8c9f84d1ed5393ad18da2fe52285ea7cbb
swimbott <bott0137@umn.edu>
2023-04-19 20:43:29 -0500

    morning rush hour stats

--- arcpy/qaqc/Historic_PurpleAir/Summary_Functions.py
+++ arcpy/qaqc/Historic_PurpleAir/Summary_Functions.py
@@ -49,3 +49,27 @@ summary_stats_dtypes += [float, float] # Stat Value Types
 summary_stats_functions += [getminmax]
 
 # ~~~~~
+
+# Stats for Morning rush hour
+
+df['timestamp'] = pd.to_datetime(df.timestamp)
+
+def pm_25_MorningRushStats(df):
+    df_6_to_9 = df.loc[(df['timestamp'].dt.hour >= 6) & (df['timestamp'].dt.hour < 9)]
+    
+    pm25_morningRushmin = df_6_to_9['pm25'].min()
+    pm25_morningRushmax = df_6_to_9['pm25'].max()
+    pm25_morningRushmean = df_6_to_9['pm25'].mean()
+    pm25_morningRushstd = df_6_to_9['pm25'].std()
+    
+    return [pm25_morningRushmean, pm25_morningRushmin, pm25_morningRushmax, pm25_morningRushstd]
+
+# Add to storage
+
+summary_stats_names += ['pm25_morningRushmean', 'pm25_morningRushmin', 'pm25_morningRushmax','pm25_morningRushstd'] # Stat Names
+
+summary_stats_dtypes += [float, float, float, float] # Stat Value Types
+
+summary_stats_functions += [pm_25_MorningRushStats]
+
+# ~~~~~
